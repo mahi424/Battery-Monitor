dc7b6ed398bbf090ffe001c6baec5123991ac870
Mahaveer Chouhan <mahaveerchouhan424@gmail.com>
2018-01-06 00:07:16 +0530

    Add files via upload

---
+++ batteryfull.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+while true
+do
+    export DISPLAY=:0.0
+    battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+    if on_ac_power; then
+        if [ "$battery_percent" -gt 95 ]; then
+            notify-send -i "$PWD/batteryfull.png" "Battery full." "Level: ${battery_percent}% "
+        fi
+    else
+
+		if [ "$battery_percent" -lt 20 ]; then
+	            notify-send   "Plug in the Charger." "Level: ${battery_percent}% "
+	    fi
+	fi
+    
+    sleep 300 # (5 minutes)
+done
